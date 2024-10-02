b128a6798c0f18638cd1d9ff4ede6dacb3aa8dc2
NhanHuynhtv0104 <hieunhantv2002@gmail.com>
2024-10-02 13:30:21 +0700

    create imgsz when export

--- export_model_to_openvino.py
+++ export_model_to_openvino.py
@@ -4,14 +4,15 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Example of argument passing")
 parser.add_argument("--model", type=str, help="path of your model, if using model yolo, it will be auto downloaded", default="yolov8n.pt")
+parser.add_argument("--imgsz",type=str, help="image input size", default="640")
 parser.add_argument("--dynamic", action="store_true", help="using model with dynamic input size")
 parser.add_argument("--half", action="store_true", help="quantization model to float16(FP16)")
 parser.add_argument("--int8", action="store_true", help="quantization model to int8")
 parser.add_argument("--batch-size", type=int, help="batch size for inference", default=1)
 args = parser.parse_args()
 
-model_path, dynamic, half, int8, batch_size = vars(args).values()
+model_path, imgsz,dynamic, half, int8, batch_size = vars(args).values()
 
 print(f"model: {model_path}, dynamic: {dynamic}, half: {half}, int8: {int8}, batch size: {batch_size}")
 model = YOLO(model_path)
-model.export(format="openvino",dynamic=dynamic, half=half, int8=int8, batch = batch_size)
+model.export(format="openvino",imgsz=imgsz,dynamic=dynamic, half=half, int8=int8, batch = batch_size)
